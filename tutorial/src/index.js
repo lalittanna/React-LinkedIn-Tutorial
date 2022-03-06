e4f94454e7ed1f0093b38e8d22f9d84ea038a4eb
lalit <lalittanna77@gmail.com>
2022-03-06 19:25:28 +0000

    Finished
    Finished and build the project

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect, useReducer } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App({ login }) {
+  const [data, setData] = useState(null);
+  const [checked, toggle] = useReducer((checked) => !checked, false);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  });
+
+  if (data) {
+    return (
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} width={100} />
+
+        <br />
+
+        <input type="checkbox" value={checked} onClick={toggle} />
+        {checked ? "checked" : "not checked"}
+      </div>
+    );
+  }
+
+  return null;
+}
+
+ReactDOM.render(<App login="lalittanna" />, document.getElementById("root"));
